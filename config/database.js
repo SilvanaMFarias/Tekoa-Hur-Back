e7ba6d554c1ebeefb2183781fe17a96b2bc91343
adramarilla <adriana.amarilla@unahur.edu.ar>
2026-04-14 15:55:52 -0300

    fix conexion db dokploy

--- config/database.js
+++ config/database.js
@@ -1,17 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const dbUrl = new URL(
-  process.env.DATABASE_URL || "postgres://postgres:2850@localhost:5432/tekoadb"
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+  }
 );
-
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: dbUrl.hostname,
-  port: dbUrl.port,
-  username: dbUrl.username,
-  password: dbUrl.password,
-  database: dbUrl.pathname.slice(1),
-  logging: false,
-});
 
 module.exports = sequelize;
