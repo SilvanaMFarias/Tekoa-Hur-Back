8588720c669989b4a6b0e94075a1a88606f99fdd
adramarilla <adriana.amarilla@unahur.edu.ar>
2026-04-15 00:03:15 -0300

    Cambio index.js para dokploy

--- models/index.js
+++ models/index.js
@@ -9,11 +9,35 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+//let sequelize;
+//if (config.use_env_variable) {
+//  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+//} else {
+//  sequelize = new Sequelize(config.database, config.username, config.password, config);
+//}
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.DB_HOST) {
+  // 👉 PRODUCCIÓN (Dokploy)
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // 👉 DESARROLLO (local)
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs
