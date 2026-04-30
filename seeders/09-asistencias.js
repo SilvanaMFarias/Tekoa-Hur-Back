b6fd127cda5419f880a6b170f7ee5693961fb07d
chamiamarillo <yamil.amarillo@unahur.edu.ar>
2026-04-30 09:59:00 -0300

    feat: modificacion de seeders para prueba de POC

--- seeders/09-asistencias.js
+++ seeders/09-asistencias.js
@@ -9,10 +9,10 @@ module.exports = {
         asistenciaId: randomUUID(),
         fecha: '2026-04-22', // new Date()
         tipoUsuario: 'ESTUDIANTE',
-        usuarioId: '77777775',
+        usuarioId: '77777777',
         horaRegistro: '16:07:00',
         estado: 'PRESENTE',
-        comisionId: '51f1afee-28ed-435e-a1ba-75baa87061ff',
+        comisionId: '6ccdf17f-b8ce-440e-a20a-97e50dbb8e96',
         createdAt: new Date(),
         updatedAt: new Date()
       }
