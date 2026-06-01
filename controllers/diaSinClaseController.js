d0d4c501154612dea20ae9c7c94db75970fcd2dc
Tomas Miranda <tomasmiranda98@hotmail.com>
2026-06-01 18:54:04 -0300

    cambios para mejorar performance

--- controllers/diaSinClaseController.js
+++ controllers/diaSinClaseController.js
@@ -6,6 +6,23 @@ class DiaSinClaseController extends BaseController {
   constructor() {
     super(diaSinClaseService);
   }
+  getFormData = async (req, res) => {
+  try {
+    const data = await this.service.getFormData();
+
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error(
+      "Error en DiaSinClaseController.getFormData:",
+      error
+    );
+
+    return res.status(500).json({
+      error:
+        "Error interno al recopilar los datos para el formulario."
+    });
+  }
+};
 }
 
 module.exports = new DiaSinClaseController();
--- services/diaSinClaseService.js
+++ services/diaSinClaseService.js
@@ -3,12 +3,12 @@ const BaseService = require("./baseService");
 const {
   DiaSinClase,
   TipoEvento,
-  Comision
+  Comision,
+  Materia // Materia para el dropdown
 } = require("../models");
 
 class DiaSinClaseService extends BaseService {
   constructor() {
-
     // Includes automáticos
     super(DiaSinClase, [
       {
@@ -20,6 +20,25 @@ class DiaSinClaseService extends BaseService {
         as: "comision",
       }
     ]);
+  }
+
+  async getFormData() {
+    // Ejecuta las consultas en paralelo usando Sequelize (findAll)
+    const [materias, comisiones, diasSinClase, tipoEventos] = await Promise.all([
+      Materia.findAll(),
+      Comision.findAll({
+        include: [{ model: Materia, as: "materia" }] // Así NextJS puede hacer c.materia?.nombre
+      }),
+      DiaSinClase.findAll(),
+      TipoEvento.findAll()
+    ]);
+
+    return {
+      materias,
+      comisiones,
+      diasSinClase,
+      tipoEventos
+    };
   }
 }
 
